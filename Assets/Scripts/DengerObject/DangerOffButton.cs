1ed38f492b8ec1a481d50ffa5753a810ebe9ca1c
DenisPlaksia <denisplaksia@gmail.com>
2020-11-29 18:34:54 +0300

    Some changes

---
+++ Assets/Scripts/DengerObject/DangerOffButton.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class DangerOffButton : MonoBehaviour
+{
+
+    [SerializeField] private GameObject _dangerObjectOff;
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.GetComponent<Player>() != null)
+        {
+            Destroy(_dangerObjectOff);
+            Destroy(this);
+        }
+    }
+}
